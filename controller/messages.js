49218e3dac649ed41d66af832d609fa785e89978
Sthitapragyan Parida <sthitapragyanparida@Sthitapragyans-MacBook-Pro.local>
2022-03-24 13:34:31 +0530

    Initial DB Setup

--- controller/messages.js
+++ controller/messages.js
@@ -1,26 +1,37 @@
-let messages = [
-    {
-        id: 1,
-        message: 'Test Message 1'
-    },
-    {
-        id: 2,
-        message: 'Test Message 2'
-    },
-    {
-        id: 3,
-        message: 'Test Message 3'
+const knex = require('knex')({
+    client: 'mysql2',
+    connection: {
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'root',
+      password : 'MyRootPa$$word',
+      database : 'mysql_test_db'
     }
-]
+  });
+
+// let messages = [
+//     {
+//         id: 1,
+//         message: 'Test Message 1'
+//     },
+//     {
+//         id: 2,
+//         message: 'Test Message 2'
+//     },
+//     {
+//         id: 3,
+//         message: 'Test Message 3'
+//     }
+// ]
 
 const getAllMessages = async (req, reply) => {
-    return messages
+    return knex('messages_table').select()
 }
 
 const getMessage = async (req, reply) => {
-    const id = Number(req.params.id)
-    const message = messages.find(message => message.id === id)
-    return message
+    //const id = Number(req.params.id)
+    //console.log("Hello World", id)
+    return knex('messages_table').select()
 }
 
 const addMessage = async (req, reply) => {
